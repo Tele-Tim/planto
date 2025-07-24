e095b2283dca7e5c147fe952663ac6ca5bbff80e
Timofei Mamaev <timofeiisrael@gmail.com>
2025-07-24 22:10:22 +0300

    changed sendRefreshCookie in AuthServiceImpl not for production

--- src/main/java/planto_project/security/JwtAuthenticationFilter.java
+++ src/main/java/planto_project/security/JwtAuthenticationFilter.java
@@ -32,11 +32,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-//        String path = request.getServletPath();
-        return HttpMethod.OPTIONS.matches(request.getMethod());
-//                || path.equals("/auth/login")
-//                || path.equals("/auth/refresh")
-//                || path.equals("/auth/logout");
+        String path = request.getServletPath();
+        return HttpMethod.OPTIONS.matches(request.getMethod()) || path.equals("/auth/login")
+                || path.equals("/auth/refresh") || path.equals("/auth/logout");
     }
 
     @Override
